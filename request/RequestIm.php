87dec895e816afe2fc3b63dbc0f8d7886e7e972f
zhengtaoyu <717828758@qq.com>
2019-02-25 15:07:37 +0800

    1

---
+++ request/RequestIm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyr
+ * Date: 2019/2/22
+ * Time: 14:58
+ */
+
+namespace cyr\tencentim\request;
+
+
+class RequestIm
+{
+    public function test(){
+        echo "this is im test;";
+    }
+}
